e5fa3b7be7126883f704e45750fa2b9f51821a0a
Marie <marie.199204@gmail.com>
2024-01-11 01:14:32 +0900

    base設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
+// eslint-disable-next-line
+//import '@/styles/globals.css'
+// eslint-disable-next-line
+import '@/styles/reset.css'
+import { CssBaseline, ThemeProvider as MUIThemeProvider } from '@mui/material'
+
+import { theme } from '@/utils/themeConfigs'
+
+import { Meta } from '@/components/Other/Meta'
+
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <MUIThemeProvider theme={theme}>
+      <CssBaseline />
+      <Meta />
+      <Component {...pageProps} />
+    </MUIThemeProvider>
+  )
 }
+
+export default App
