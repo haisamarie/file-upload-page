bb193d1941cb91392146e5b0041f3ad389939ab4
Marie <marie.199204@gmail.com>
2024-01-11 00:45:13 +0900

    topのdefolt削除

---
+++ src/utils/themeConfigs/customTheme.ts
@@ -0,0 +1,66 @@
+const color = {
+  background: {
+    lightAqua: '#E0EFFF',
+    aqua: '#93CBFF',
+    darkAqua: '#349BF9',
+    lightBlue:
+      'linear-gradient(0deg, rgba(147, 203, 255, 0.7) 0%, rgba(147, 203, 255, 0.7) 100%)',
+    blue: '#1C7AE8',
+    white: '#FFFFFF',
+    darkGray: '#A0A0A0',
+    gray: '#E7E7E7',
+    lightGray: '#F7F5F5',
+    green: '#80C739',
+    rightRed: '#F9CFCF',
+    red: '#EB5554',
+  },
+  text: {
+    darkAqua: '#349BF9',
+    darkBlue: '#051ADF',
+    blue: '#1C7AE8',
+    darkRed: '#D32827',
+    white: '#FFFFFF',
+    gray: '#A0A0A0',
+    darkGray: '#565656',
+    black: '#333333',
+    error: '#d32f2f',
+  },
+  border: {
+    darkAqua: '#349BF9',
+    darkBlue: '#051ADF',
+    blue: '#1C7AE8',
+    black: '#333333',
+    gray: '#D4D4D4',
+    lightGray: '#D8D8DB',
+    darkGray: '#A0A0A0',
+    darkRed: '#D32827',
+    error: '#d32f2f',
+  },
+}
+
+const font = {
+  size: {
+    xxxs: '10px',
+    xxs: '12px',
+    xs: '14px',
+    sm: '16px',
+    md: '18px',
+    lg: '20px',
+    xl: '24px',
+    xxl: '28px',
+  },
+  weight: {
+    light: '300',
+    normal: '400',
+    bold: '700',
+  },
+}
+
+// color
+export const bgColor = color.background
+export const textColor = color.text
+export const borderColor = color.border
+
+// font
+export const fontSize = font.size
+export const fontWeight = font.weight
