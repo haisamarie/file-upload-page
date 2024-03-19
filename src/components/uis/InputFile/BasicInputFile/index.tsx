b928b07c6674bf5015498ffc602acea87ed87f4f
Marie <marie.199204@gmail.com>
2024-03-19 21:02:59 +0900

    ファイルアップされた後の処理

--- src/components/uis/InputFile/BasicInputFile/index.tsx
+++ src/components/uis/InputFile/BasicInputFile/index.tsx
@@ -1,13 +1,29 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 
 import styles from './styles.module.scss'
 
 export const BasicInputFile = (): JSX.Element => {
   const fileInputRef = useRef<HTMLInputElement>(null)
+  const [fileName, setFileName] = useState<string>('')
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const fileList = event.target.files
+    if (fileList && fileList.length > 0) {
+      const uploadedFile = fileList[0]
+      setFileName(uploadedFile.name)
+    }
+  }
 
   const handleButtonClick = () => {
     if (fileInputRef.current) {
       fileInputRef.current.click()
+    }
+  }
+
+  const handleFileRemove = () => {
+    setFileName('')
+    if (fileInputRef.current) {
+      fileInputRef.current.value = ''
     }
   }
   return (
@@ -16,12 +32,30 @@ export const BasicInputFile = (): JSX.Element => {
         <p className={styles['text']}>
           ※ファイルを選択するかドラックアンドドロップしてください
         </p>
-        <input ref={fileInputRef} type='file' className={styles['input-file']} />
+        <input
+          ref={fileInputRef}
+          type='file'
+          className={styles['input-file']}
+          onChange={handleFileChange}
+        />
         <button className={styles['file-button']} onClick={handleButtonClick}>
           ファイルを選択
         </button>
       </div>
       <p className={styles['note']}>※1ファイルの最大サイズは50MBです</p>
+
+      <div className={styles['file-up-wrap']}>
+        {fileName ? (
+          <>
+            <span>{fileName}</span>
+            <button onClick={handleFileRemove} className={styles['remove-button']}>
+              ×
+            </button>
+          </>
+        ) : (
+          <span className={styles['default-file-text']}>ファイルを選択してください</span>
+        )}
+      </div>
     </div>
   )
 }
